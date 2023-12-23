5efd950be29b4362af6479a7f64c730c85b27328
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-24 00:15:32 +0900

    [Feat] User 스키마 추가

---
+++ domain/user/user_schema.py
@@ -0,0 +1,35 @@
+import datetime
+
+from pydantic import BaseModel, field_validator, EmailStr
+from pydantic_core.core_schema import FieldValidationInfo
+
+
+class UserCreate(BaseModel):
+    name: str
+    password1: str
+    password2: str
+    email: EmailStr
+
+    @field_validator("name", "password1", "password2", "email")
+    def is_not_empty(cls, value: str):
+        if not value.strip():
+            raise ValueError("값이 공백일 수 없습니다.")
+        return value
+
+    @field_validator("password2")
+    def password_confirm(cls, value, info: FieldValidationInfo):
+        if "password1" in info.data and value != info.data["password1"]:
+            raise ValueError("비밀번호가 일치하지 않습니다")
+        return value
+
+
+class UserToken(BaseModel):
+    access_token: str
+    token_type: str
+    user_name: str
+
+
+class UserReadDetail(BaseModel):
+    name: str
+    email: EmailStr
+    join_date: datetime.datetime
