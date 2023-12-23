f4eb4cd3875b9c9d4773dc7b3e96d774df7bbb9a
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-24 00:17:41 +0900

    [Feat] main에 User 라우터 연결, 미들웨어 추가, 서버 실행 바로가기 추가

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+
+from domain.user import user_router
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(user_router.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True, log_level="debug")
