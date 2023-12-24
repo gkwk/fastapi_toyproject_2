3ee49445897a1ba37b57c32328da850c7cc76123
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-25 04:18:24 +0900

    [Feat] database User 생성 테스트 코드 작성

---
+++ database_test.py
@@ -0,0 +1,26 @@
+from models import User
+from domain.user.user_schema import UserCreate
+from domain.user.user_crud import create_user
+from datetime import datetime
+from database import session_local
+
+data_base = session_local()
+
+try:
+    data_base.query(User).delete()
+    data_base.commit()
+    
+    limit = 50
+    
+    for i in range(limit):
+        name = f"test{i}"
+        email = f"test{i}@test.com"
+        password = f"{i}"
+        schema = UserCreate(name=name, password1=password, password2=password, email=email)
+
+        create_user(data_base=data_base, user_create=schema)
+        print(f"\rUser : {i+1}/{limit}", end="")
+except:
+    pass
+data_base.commit()
+data_base.close()
