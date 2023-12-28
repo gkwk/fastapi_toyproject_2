20280384f7d490d84e479be83ec7a52f4025321b
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-28 18:10:58 +0900

    [Feat] User 관련 라우팅 기능 추가 및 수정

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -16,27 +16,11 @@ router = APIRouter(
 )
 
 
-@router.get("/detail", response_model=user_schema.UserReadDetail)
-def get_user_detail(
-    token=Depends(get_oauth2_scheme()), data_base: Session = Depends(get_data_base)
+@router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
+def create_user(
+    schema: user_schema.UserCreate, data_base: Session = Depends(get_data_base)
 ):
-    data = check_and_decode_user_token(token=token, data_base=data_base)
-
-    return user_crud.get_user_detail(
-        data_base=data_base, user_name=data["user_name"], user_id=data["user_id"]
-    )
-
-
-@router.post("/register", status_code=status.HTTP_204_NO_CONTENT)
-def register_user(
-    user_create: user_schema.UserCreate, data_base: Session = Depends(get_data_base)
-):
-    if user_crud.does_user_already_exist(data_base=data_base, user_create=user_create):
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="동일한 이름 혹은 이메일을 사용중인 사용자가 이미 존재합니다.",
-        )
-    user_crud.create_user(data_base=data_base, user_create=user_create)
+    user_crud.create_user(data_base=data_base, schema=schema)
 
 
 @router.post("/login", response_model=user_schema.UserToken)
@@ -45,3 +29,34 @@ def login_user(
     data_base: Session = Depends(get_data_base),
 ):
     return generate_user_token(form_data=form_data, data_base=data_base)
+
+
+@router.get("/detail", response_model=user_schema.UserDetail)
+def get_user_detail(
+    token=Depends(get_oauth2_scheme()), data_base: Session = Depends(get_data_base)
+):
+    data = check_and_decode_user_token(token=token, data_base=data_base)
+
+    return user_crud.get_user_with_id_and_name(
+        data_base=data_base, user_name=data["user_name"], user_id=data["user_id"]
+    )
+
+
+@router.post("/update_detail", status_code=status.HTTP_204_NO_CONTENT)
+def update_user_detail(
+    schema: user_schema.UserUpdate,
+    token=Depends(get_oauth2_scheme()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token, data_base=data_base)
+    user_crud.update_user(data_base=data_base, schema=schema, decoded_token=data)
+
+
+@router.post("/update_password", status_code=status.HTTP_204_NO_CONTENT)
+def update_user_detail(
+    schema: user_schema.UserUpdatePassword,
+    token=Depends(get_oauth2_scheme()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token, data_base=data_base)
+    user_crud.update_user_password(data_base=data_base, schema=schema, decoded_token=data)
