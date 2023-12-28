11d859abf04011479a78790fc33e7e7e550c482f
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-28 18:10:05 +0900

    [Feat] User 관련 스키마 추가 및 수정

--- domain/user/user_schema.py
+++ domain/user/user_schema.py
@@ -23,13 +23,51 @@ class UserCreate(BaseModel):
         return value
 
 
+class UserReadWithEmailAndName(BaseModel):
+    name: str
+    email: EmailStr
+
+    @field_validator("name", "email")
+    def is_not_empty(cls, value: str):
+        if not value.strip():
+            raise ValueError("값이 공백일 수 없습니다.")
+        return value
+
+
+class UserUpdate(BaseModel):
+    email: EmailStr
+
+    @field_validator("email")
+    def is_not_empty(cls, value: str):
+        if not value.strip():
+            raise ValueError("값이 공백일 수 없습니다.")
+        return value
+
+
+class UserUpdatePassword(BaseModel):
+    password1: str
+    password2: str
+
+    @field_validator("password1", "password2")
+    def is_not_empty(cls, value: str):
+        if not value.strip():
+            raise ValueError("값이 공백일 수 없습니다.")
+        return value
+
+    @field_validator("password2")
+    def password_confirm(cls, value, info: FieldValidationInfo):
+        if "password1" in info.data and value != info.data["password1"]:
+            raise ValueError("비밀번호가 일치하지 않습니다")
+        return value
+
+
 class UserToken(BaseModel):
     access_token: str
     token_type: str
     user_name: str
 
 
-class UserReadDetail(BaseModel):
+class UserDetail(BaseModel):
     name: str
     email: EmailStr
     join_date: datetime.datetime
