18c939e39746feed5569652de65a2b671a52eecf
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-29 19:54:55 +0900

    [Feat] admin ban 관련 라우팅 추가

--- domain/admin/admin_router.py
+++ domain/admin/admin_router.py
@@ -12,14 +12,16 @@ router = APIRouter(
 )
 
 
-@router.get("/users", response_model=admin_schema.UserDetailList)
+@router.get("/users", response_model=admin_schema.UserMoreDetailList)
 def get_users(
-    token=Depends(get_oauth2_scheme(is_admin=True)), data_base: Session = Depends(get_data_base)
+    token=Depends(get_oauth2_scheme(is_admin=True)),
+    data_base: Session = Depends(get_data_base),
 ):
     data = admin_crud.check_and_decode_admin_token(token=token, data_base=data_base)
     users = admin_crud.get_users(data_base=data_base)
 
-    return {"users" : users}
+    return {"users": users}
+
 
 @router.post("/login", response_model=admin_schema.AdminToken)
 def login_admin(
@@ -27,3 +29,14 @@ def login_admin(
     data_base: Session = Depends(get_data_base),
 ):
     return admin_crud.generate_admin_token(form_data=form_data, data_base=data_base)
+
+
+@router.post("/update_user_is_banned", status_code=status.HTTP_204_NO_CONTENT)
+def update_user_is_banned(
+    schema: admin_schema.UserBanOption,
+    token=Depends(get_oauth2_scheme(is_admin=True)),
+    data_base: Session = Depends(get_data_base),
+):
+    data = admin_crud.check_and_decode_admin_token(token=token, data_base=data_base)
+
+    return admin_crud.update_user_is_banned(data_base=data_base, schema=schema)
