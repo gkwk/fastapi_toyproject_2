cf4ab4b849c6dd067e22a0c66ea0d9471ac05d43
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-29 12:38:04 +0900

    [Feat] admin 로그인을 위한 함수 추가

--- domain/admin/admin_crud.py
+++ domain/admin/admin_crud.py
@@ -3,14 +3,47 @@ import secrets
 import getpass
 
 from sqlalchemy.orm import Session
+
+from datetime import timedelta, datetime
+import secrets
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from starlette import status
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+
+from models import User
+from domain.user.user_schema import (
+    UserCreate,
+    UserReadWithEmailAndName,
+    UserUpdate,
+    UserUpdatePassword,
+)
+from domain.admin.admin_schema import UserDetailList
+
+from database import get_data_base
+from config import get_settings
+
 from models import User
 from domain.user.user_crud import (
     does_user_name_already_exist,
     does_user_email_already_exist,
     get_password_context,
+    get_user_with_name,
+    get_oauth2_scheme,
+    get_user_with_id_and_name,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    SECRET_KEY,
+    ALGORITHM,
 )
 from domain.user.user_schema import UserCreate
 from database import session_local
+
+
+def get_users(data_base: Session):
+    return data_base.query(User).all()
 
 
 def create_admin():
@@ -53,3 +86,74 @@ def create_admin():
     data_base.commit()
 
     data_base.close()
+
+
+def generate_admin_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    data_base: Session = Depends(get_data_base),
+):
+    admin = get_user_with_name(data_base=data_base, user_name=form_data.username)
+
+    if not admin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="입력된 이름을 가지는 관리자가 존재하지 않습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    if not get_password_context().verify(
+        (form_data.password + admin.password_salt), admin.password
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="패스워드가 일치하지 않습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    if not admin.is_superuser:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="관리자 권한이 존재하지 않습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    data = {
+        "sub": "admin_token",
+        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+        "admin_name": admin.name,
+        "admin_id": admin.id,
+    }
+    access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_name": admin.name,
+        "is_admin" : True,
+    }
+
+
+def check_and_decode_admin_token(
+    token=Depends(get_oauth2_scheme()), data_base: Session = Depends(get_data_base)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="토큰이 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        admin_name: str = payload.get("admin_name")
+        admin_id: int = payload.get("admin_id")
+        if (admin_name is None) or (admin_id is None):
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    else:
+        admin = get_user_with_id_and_name(
+            data_base, user_name=admin_name, user_id=admin_id
+        )
+        if (admin is None) or (not admin.is_superuser):
+            raise credentials_exception
+
+    return payload
