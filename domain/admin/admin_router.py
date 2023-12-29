7b345d2ebbd40b1079a090dca5b2569b2c4e619f
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-30 02:46:39 +0900

    [Refactor] 코드 정리

--- domain/admin/admin_router.py
+++ domain/admin/admin_router.py
@@ -5,7 +5,11 @@ from starlette import status
 
 from database import get_data_base
 from domain.admin import admin_schema, admin_crud
-from domain.user.user_crud import get_oauth2_scheme
+from auth import (
+    get_oauth2_scheme_admin,
+    check_and_decode_admin_token,
+    generate_admin_token,
+)
 
 router = APIRouter(
     prefix="/api/admin",
@@ -14,10 +18,10 @@ router = APIRouter(
 
 @router.get("/users", response_model=admin_schema.UserMoreDetailList)
 def get_users(
-    token=Depends(get_oauth2_scheme(is_admin=True)),
+    token=Depends(get_oauth2_scheme_admin()),
     data_base: Session = Depends(get_data_base),
 ):
-    data = admin_crud.check_and_decode_admin_token(token=token, data_base=data_base)
+    data = check_and_decode_admin_token(token=token)
     users = admin_crud.get_users(data_base=data_base)
 
     return {"users": users}
@@ -28,15 +32,15 @@ def login_admin(
     form_data: OAuth2PasswordRequestForm = Depends(),
     data_base: Session = Depends(get_data_base),
 ):
-    return admin_crud.generate_admin_token(form_data=form_data, data_base=data_base)
+    return generate_admin_token(form_data=form_data, data_base=data_base)
 
 
 @router.post("/update_user_is_banned", status_code=status.HTTP_204_NO_CONTENT)
 def update_user_is_banned(
     schema: admin_schema.UserBanOption,
-    token=Depends(get_oauth2_scheme(is_admin=True)),
+    token=Depends(get_oauth2_scheme_admin()),
     data_base: Session = Depends(get_data_base),
 ):
-    data = admin_crud.check_and_decode_admin_token(token=token, data_base=data_base)
+    data = check_and_decode_admin_token(token=token)
 
     return admin_crud.update_user_is_banned(data_base=data_base, schema=schema)
