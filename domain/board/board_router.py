2b65755d164122a82ec73d465aeab2f42510cd37
gkwk <124476558+gkwk@users.noreply.github.com>
2024-01-03 15:57:47 +0900

    [Feat] Comment CRUD API 추가

--- domain/board/board_router.py
+++ domain/board/board_router.py
@@ -27,17 +27,20 @@ def get_post(
     data_base: Session = Depends(get_data_base),
 ):
     data = check_and_decode_user_token(token=token)
-    return board_crud.get_post_detail(data_base=data_base, post_id=post_id, board_id=board_id)
+    return board_crud.get_post_detail(
+        data_base=data_base, post_id=post_id, board_id=board_id
+    )
 
 
-@router.get("/get_posts/{board_id}", response_model=board_schema.PostsRead)
-def get_post(
+@router.get("/get_posts/{board_id}/{skip}", response_model=board_schema.PostsRead)
+def get_posts(
     board_id: int,
+    skip: int,
     token=Depends(get_oauth2_scheme_user()),
     data_base: Session = Depends(get_data_base),
 ):
     data = check_and_decode_user_token(token=token)
-    return board_crud.get_posts(data_base=data_base, board_id=board_id)
+    return board_crud.get_posts(data_base=data_base, board_id=board_id, skip=skip)
 
 
 @router.post("/create_post", status_code=status.HTTP_204_NO_CONTENT)
@@ -68,3 +71,59 @@ def delete_post(
 ):
     data = check_and_decode_user_token(token=token)
     board_crud.delete_post(data_base=data_base, schema=schema, token_payload=data)
+
+
+@router.get(
+    "/get_comment/{post_id}/{comment_id}", response_model=board_schema.CommentRead
+)
+def get_comment(
+    post_id: int,
+    comment_id: int,
+    token=Depends(get_oauth2_scheme_user()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token)
+    return board_crud.get_comment_detail(
+        data_base=data_base, post_id=post_id, comment_id=comment_id
+    )
+
+
+@router.get("/get_comments/{post_id}/{skip}", response_model=board_schema.CommentsRead)
+def get_comments(
+    post_id: int,
+    skip: int,
+    token=Depends(get_oauth2_scheme_user()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token)
+    return board_crud.get_comments(data_base=data_base, post_id=post_id, skip=skip)
+
+
+@router.post("/create_comment", status_code=status.HTTP_204_NO_CONTENT)
+def create_comment(
+    schema: board_schema.CommentCreate,
+    token=Depends(get_oauth2_scheme_user()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token)
+    board_crud.create_comment(data_base=data_base, schema=schema, token_payload=data)
+
+
+@router.put("/update_comment", status_code=status.HTTP_204_NO_CONTENT)
+def update_comment(
+    schema: board_schema.CommentUpdate,
+    token=Depends(get_oauth2_scheme_user()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token)
+    board_crud.update_comment(data_base=data_base, schema=schema, token_payload=data)
+
+
+@router.delete("/delete_comment", status_code=status.HTTP_204_NO_CONTENT)
+def delete_comment(
+    schema: board_schema.CommentDelete,
+    token=Depends(get_oauth2_scheme_user()),
+    data_base: Session = Depends(get_data_base),
+):
+    data = check_and_decode_user_token(token=token)
+    board_crud.delete_comment(data_base=data_base, schema=schema, token_payload=data)
