694086e57ec3ef7d1f3fbf27694fb8f105a1c670
gkwk <124476558+gkwk@users.noreply.github.com>
2024-01-24 00:08:06 +0900

    [Feat] Celery를 위한 DB 데코레이터 추가, 서버 종료시 DB 해제 추가

--- database.py
+++ database.py
@@ -1,15 +1,23 @@
-from typing import Annotated
+from typing import Annotated, Optional
+import contextlib
+
 from fastapi import Depends
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from config import get_settings
 
+import json
+
 
 engine = create_engine(
-    get_settings().SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    get_settings().SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
 )
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 # SQLite 버그 패치
 naming_convention = {
@@ -25,12 +33,36 @@ class Base(DeclarativeBase):
     metadata = MetaData(naming_convention=naming_convention)
 
 
+def database_engine_shutdown():
+    global engine
+    if engine:
+        engine.dispose()
+
+
 def get_data_base():
     data_base = session_local()
     try:
         yield data_base
     finally:
         data_base.close()
+
+
+@contextlib.contextmanager
+def get_data_base_for_decorator():
+    data_base = session_local()
+    try:
+        yield data_base
+    finally:
+        data_base.close()
+
+json_encoder = json.JSONEncoder()
+def get_data_base_decorator(f):
+    def wrapper(data_base, *args, **kwargs):
+        with get_data_base_for_decorator() as data_base:
+            if "data_base" in kwargs:
+                kwargs["data_base"] = data_base
+            f(data_base, *args, **kwargs)
+    return wrapper
 
 
 data_base_dependency = Annotated[Session, Depends(get_data_base)]
