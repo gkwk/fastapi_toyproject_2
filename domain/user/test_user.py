6dc746f85008472c3160cdbb28af12d370acfd7d
gkwk <124476558+gkwk@users.noreply.github.com>
2024-02-15 15:09:56 +0900

    [Test] main 및 user 테스트 코드 중복 수정 및 case 코드 추가

--- domain/user/test_user.py
+++ domain/user/test_user.py
@@ -1,10 +1,14 @@
+import pytest
+
 from fastapi.testclient import TestClient
+from httpx import Response
 
 from main import app
 from models import User
 from database import session_local
 from auth import validate_and_decode_user_access_token
 import v1_urn
+from test_main import main_test_methods
 
 client = TestClient(app)
 
@@ -34,38 +38,30 @@ URL_USER_UPDATE_USER_PASSWORD = "".join(
 )
 
 
-class TestUser:
-    def test_data_base_init(self):
-        data_base = session_local()
-        data_base.query(User).delete()
-        data_base.commit()
-        data_base.close()
-
-    def test_create_user(self):
-        data_base = session_local()
-
+class UserTestMethods:
+    def create_user(self, name, password1, password2, email):
         response_test = client.post(
             URL_USER_CREATE_USER,
             json={
-                "name": TEST_USER_ID,
-                "password1": TEST_USER_PASSWORD1,
-                "password2": TEST_USER_PASSWORD2,
-                "email": TEST_USER_EMAIL,
+                "name": name,
+                "password1": password1,
+                "password2": password2,
+                "email": email,
             },
         )
 
+        return response_test
+
+    def create_user_test(self, response_test: Response, name, password1, email):
+        data_base = session_local()
         assert response_test.status_code == 201
         assert response_test.json() == {"result": "success"}
 
-        user = (
-            data_base.query(User)
-            .filter_by(name=TEST_USER_ID, email=TEST_USER_EMAIL)
-            .first()
-        )
+        user = data_base.query(User).filter_by(name=name, email=email).first()
 
-        assert user.name == TEST_USER_ID
-        assert user.email == TEST_USER_EMAIL
-        assert user.password != None
+        assert user.name == name
+        assert user.email == email
+        assert user.password != password1
         assert user.password_salt != None
         assert user.join_date != None
         assert user.update_date == None
@@ -74,41 +70,41 @@ class TestUser:
 
         data_base.close()
 
-    def test_login_user(self):
-        data_base = session_local()
+    def login_user(self, name, password1):
 
-        response_login = client.post(
+        response_test = client.post(
             URL_USER_LOGIN_USER,
-            data={"username": TEST_USER_ID, "password": TEST_USER_PASSWORD1},
+            data={"username": name, "password": password1},
             headers={"content-type": "application/x-www-form-urlencoded"},
         )
 
-        assert response_login.status_code == 200
-        response_login_json: dict = response_login.json()
-        assert response_login_json.get("access_token")
-        assert response_login_json.get("token_type") == "bearer"
+        return response_test
+
+    def login_user_test(self, response_test: Response, name):
+        data_base = session_local()
+
+        assert response_test.status_code == 200
+        response_test_json: dict = response_test.json()
+        assert response_test_json.get("access_token")
+        assert response_test_json.get("token_type") == "bearer"
 
         user_payload = validate_and_decode_user_access_token(
-            data_base=data_base, token=response_login_json.get("access_token")
+            data_base=data_base, token=response_test_json.get("access_token")
         )
 
         assert user_payload.get("user_id") >= 1
-        assert user_payload.get("user_name") == TEST_USER_ID
+        assert user_payload.get("user_name") == name
         assert user_payload.get("is_admin") == False
+
+        user = data_base.query(User).filter_by(id=user_payload.get("user_id")).first()
+
+        assert user.name == name
 
         data_base.close()
 
-    def test_get_user_detail(self):
-        response_login = client.post(
-            URL_USER_LOGIN_USER,
-            data={"username": TEST_USER_ID, "password": TEST_USER_PASSWORD1},
-            headers={"content-type": "application/x-www-form-urlencoded"},
-        )
-
-        assert response_login.status_code == 200
+    def get_user_detail(self, name, password1):
+        response_login: Response = self.login_user(name, password1)
         response_login_json: dict = response_login.json()
-        assert response_login_json.get("access_token")
-        assert response_login_json.get("token_type")
 
         response_test = client.get(
             URL_USER_GET_USER_DETAIL,
@@ -116,74 +112,56 @@ class TestUser:
                 "Authorization": f"Bearer {response_login_json.get('access_token')}"
             },
         )
+        return response_test
+
+    def get_user_detail_test(self, response_test: Response, name, email):
+        data_base = session_local()
 
         assert response_test.status_code == 200
 
         response_test_json: dict = response_test.json()
 
-        assert response_test_json.get("name") == TEST_USER_ID
-        assert response_test_json.get("email") == TEST_USER_EMAIL
+        assert response_test_json.get("name") == name
+        assert response_test_json.get("email") == email
         assert response_test_json.get("join_date") != None
         assert response_test_json.get("boards") != None
         assert response_test_json.get("posts") != None
 
-    def test_update_user_detail(self):
-        data_base = session_local()
+        data_base.close()
 
-        response_login = client.post(
-            URL_USER_LOGIN_USER,
-            data={"username": TEST_USER_ID, "password": TEST_USER_PASSWORD1},
-            headers={"content-type": "application/x-www-form-urlencoded"},
-        )
-
-        assert response_login.status_code == 200
+    def update_user_detail(self, name, password1, email_update):
+        response_login: Response = self.login_user(name, password1)
         response_login_json: dict = response_login.json()
-        assert response_login_json.get("access_token")
-        assert response_login_json.get("token_type")
-
-        user_id = validate_and_decode_user_access_token(
-            data_base=data_base, token=response_login_json.get("access_token")
-        ).get("user_id")
-        user = data_base.query(User).filter_by(id=user_id).first()
 
         response_test = client.put(
             URL_USER_UPDATE_USER_DETAIL,
             json={
-                "email": TEST_USER_EMAIL_UPDATE,
+                "email": email_update,
             },
             headers={
                 "Authorization": f"Bearer {response_login_json.get('access_token')}"
             },
         )
-        data_base.commit()
+
+        return response_test
+
+    def update_user_detail_test(self, response_test: Response, name, email_update):
+        data_base = session_local()
 
         assert response_test.status_code == 204
 
-        user = data_base.query(User).filter_by(id=user_id).first()
+        user = data_base.query(User).filter_by(name=name).first()
 
-        assert user.name == TEST_USER_ID
-        assert user.email == TEST_USER_EMAIL_UPDATE
-        assert user.password != None
-        assert user.password_salt != None
-        assert user.join_date != None
-        assert user.is_superuser == False
-        assert user.is_banned == False
+        assert user.email == email_update
+        assert user.update_date != None
 
         data_base.close()
 
-    def test_update_user_password(self):
+    def update_user_password(self, name, password1, password1_update, password2_update):
         data_base = session_local()
 
-        response_login = client.post(
-            URL_USER_LOGIN_USER,
-            data={"username": TEST_USER_ID, "password": TEST_USER_PASSWORD1},
-            headers={"content-type": "application/x-www-form-urlencoded"},
-        )
-
-        assert response_login.status_code == 200
+        response_login: Response = self.login_user(name, password1)
         response_login_json: dict = response_login.json()
-        assert response_login_json.get("access_token")
-        assert response_login_json.get("token_type")
 
         user_id = validate_and_decode_user_access_token(
             data_base=data_base, token=response_login_json.get("access_token")
@@ -195,42 +173,104 @@ class TestUser:
         response_test = client.put(
             URL_USER_UPDATE_USER_PASSWORD,
             json={
-                "password1": TEST_USER_PASSWORD1_UPDATE,
-                "password2": TEST_USER_PASSWORD2_UPDATE,
+                "password1": password1_update,
+                "password2": password2_update,
             },
             headers={
                 "Authorization": f"Bearer {response_login_json.get('access_token')}"
             },
         )
-        data_base.commit()
+
+        data_base.close()
+
+        return {
+            "response_test": response_test,
+            "user_password_salt_old": user_password_salt_old,
+            "user_id": user_id,
+        }
+
+    def update_user_password_test(
+        self,
+        name,
+        password1_update,
+        response_test: Response,
+        user_password_salt_old,
+        user_id,
+    ):
+        data_base = session_local()
 
         assert response_test.status_code == 204
 
-        response_login = client.post(
-            URL_USER_LOGIN_USER,
-            data={"username": TEST_USER_ID, "password": TEST_USER_PASSWORD1_UPDATE},
-            headers={"content-type": "application/x-www-form-urlencoded"},
-        )
-
-        assert response_login.status_code == 200
+        response_login: Response = self.login_user(name, password1_update)
         response_login_json: dict = response_login.json()
-        assert response_login_json.get("access_token")
-        assert response_login_json.get("token_type")
 
         user_id = validate_and_decode_user_access_token(
             data_base=data_base, token=response_login_json.get("access_token")
         ).get("user_id")
 
         user = data_base.query(User).filter_by(id=user_id).first()
+
         user_password_salt_new = user.password_salt
 
-        assert user.name == TEST_USER_ID
-        assert user.email == TEST_USER_EMAIL_UPDATE
-        assert user.password != None
+        assert user.name == name
         assert user.password_salt != None
         assert user_password_salt_new != user_password_salt_old
-        assert user.join_date != None
-        assert user.is_superuser == False
-        assert user.is_banned == False
+        assert user.update_date != None
 
         data_base.close()
+
+
+user_test_methods = UserTestMethods()
+
+
+class TestUser:
+    def test_data_base_init(self):
+        main_test_methods.data_base_init()
+
+    @pytest.mark.parametrize(
+        "name, password1, password2, email",
+        [(f"user{i}", "12345678", "12345678", f"user{i}@test.com") for i in range(10)],
+    )
+    def test_create_user(self, name, password1, password2, email):
+        response_test = user_test_methods.create_user(name, password1, password2, email)
+        user_test_methods.create_user_test(response_test, name, password1, email)
+
+    @pytest.mark.parametrize(
+        "name, password1",
+        [(f"user{i}", "12345678") for i in range(10)],
+    )
+    def test_login_user(self, name, password1):
+        response_test = user_test_methods.login_user(name, password1)
+        user_test_methods.login_user_test(response_test, name)
+
+    @pytest.mark.parametrize(
+        "name, password1, email",
+        [(f"user{i}", "12345678", f"user{i}@test.com") for i in range(10)],
+    )
+    def test_get_user_detail(self, name, password1, email):
+        response_test = user_test_methods.get_user_detail(name, password1)
+        user_test_methods.get_user_detail_test(response_test, name, email)
+
+    @pytest.mark.parametrize(
+        "name, password1, email_update",
+        [(f"user{i}", "12345678", f"user{i}_update@test.com") for i in range(10)],
+    )
+    def test_update_user_detail(self, name, password1, email_update):
+        response_test = user_test_methods.update_user_detail(
+            name, password1, email_update
+        )
+        user_test_methods.update_user_detail_test(response_test, name, email_update)
+
+    @pytest.mark.parametrize(
+        " name, password1, password1_update, password2_update",
+        [(f"user{i}", "12345678", "12345679", "12345679") for i in range(10)],
+    )
+    def test_update_user_password(
+        self, name, password1, password1_update, password2_update
+    ):
+        response_dict = user_test_methods.update_user_password(
+            name, password1, password1_update, password2_update
+        )
+        user_test_methods.update_user_password_test(
+            name, password1_update, **response_dict
+        )
--- test_main.py
+++ test_main.py
@@ -2,39 +2,18 @@ from pytest import MonkeyPatch
 
 from fastapi.testclient import TestClient
 
-from sqlalchemy import select,delete,text
+from sqlalchemy import select, delete, text
 
 from main import app
 import models
-from database import session_local,engine
+from database import session_local
 
 
 client = TestClient(app)
 
 
-class TestMain:
-    def test_patch(self):
-        def patch_task():
-            a = input()
-            b = input()
-            return (a, b)
-
-        fake_input = iter(["13", "39", "44", "df"]).__next__
-        monkeypatch = MonkeyPatch()
-        monkeypatch.setattr("builtins.input", fake_input)
-
-        assert patch_task() == ("13", "39")
-        assert patch_task() == ("44", "df")
-        
-    def test_add_task(self, celery_app, celery_worker):
-        @celery_app.task
-        def mul(x, y):
-            return x * y
-
-        celery_worker.reload()
-        assert mul.delay(2, 2).get() == 4
-
-    def test_data_base_init(self):
+class MainTestMethods:
+    def data_base_init(self):
         data_base = session_local()
         data_base.execute(delete(models.JWTAccessTokenBlackList))
         data_base.execute(delete(models.JWTRefreshTokenList))
@@ -50,14 +29,54 @@ class TestMain:
         data_base.execute(delete(models.Post))
         data_base.execute(delete(models.Board))
         data_base.execute(delete(models.User))
-        
-        for value in data_base.execute(text('SELECT * FROM sqlite_sequence')).all():
-            data_base.execute(text(f'UPDATE sqlite_sequence SET seq = 0 WHERE name = "{value[0]}"'))
+
+        for value in data_base.execute(text("SELECT * FROM sqlite_sequence")).all():
+            data_base.execute(
+                text(f'UPDATE sqlite_sequence SET seq = 0 WHERE name = "{value[0]}"')
+            )
 
         data_base.commit()
         data_base.close()
 
-    def test_read_main(self):
+    def patch(self):
+        def patch_task():
+            a = input()
+            b = input()
+            return (a, b)
+
+        fake_input = iter(["13", "39", "44", "df"]).__next__
+        monkeypatch = MonkeyPatch()
+        monkeypatch.setattr("builtins.input", fake_input)
+
+        assert patch_task() == ("13", "39")
+        assert patch_task() == ("44", "df")
+
+    def add_task(self, celery_app, celery_worker):
+        @celery_app.task
+        def mul(x, y):
+            return x * y
+
+        celery_worker.reload()
+        assert mul.delay(2, 2).get() == 4
+
+    def read_main(self):
         response = client.get("/")
         assert response.status_code == 200
         assert response.json() == {"message": "Hello, FastAPI!"}
+
+
+main_test_methods = MainTestMethods()
+
+
+class TestMain:
+    def test_patch(self):
+        main_test_methods.patch()
+
+    def test_add_task(self, celery_app, celery_worker):
+        main_test_methods.add_task(celery_app=celery_app, celery_worker=celery_worker)
+
+    def test_data_base_init(self):
+        main_test_methods.data_base_init()
+
+    def test_read_main(self):
+        main_test_methods.read_main()
