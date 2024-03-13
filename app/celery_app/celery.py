d65a72520ebfcfa5a0d8cfe1e0712ecb22c0d1a1
gkwk <124476558+gkwk@users.noreply.github.com>
2024-03-13 13:19:19 +0900

    [Chore] Dockerfile 및 docker-compose 작성

--- app/celery_app/celery.py
+++ app/celery_app/celery.py
@@ -6,7 +6,9 @@ from celery import Celery
 import domain.ai
 import models
 
-celery_app = Celery(__name__, backend="db+sqlite:///celery.sqlite", broker="amqp://guest:guest@localhost:5672",)
+celery_app_broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672")
+
+celery_app = Celery(__name__, backend="db+sqlite:///celery.sqlite", broker=celery_app_broker_url,)
 # celery_app.conf.broker_url = os.environ.get(
 #     "CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672"
 # )
--- app/conftest.py
+++ app/conftest.py
@@ -1,9 +1,10 @@
+import os
 import pytest
 
-
+celery_app_broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672")
 @pytest.fixture(scope="session")
 def celery_config():
     return {
-        "broker_url": "amqp://",
+        "broker_url": celery_app_broker_url,
         "result_backend": "db+sqlite:///celery.sqlite",
     }
