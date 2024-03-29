eb55cd9f53136f80ef761d9b4c97c1173bfeedb0
gkwk <124476558+gkwk@users.noreply.github.com>
2024-03-29 23:10:47 +0900

    [Feat] Alembic 템플릿 추가 및 db 구성 자동화 관련 sh 추가 Docker compose 설정 변경

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY ./app /code/app
 
+RUN chmod +x /code/app/init.sh
+
 WORKDIR /code/app
 
 # CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ app/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DB_FILE=./db/test.sqlite
+ALEMBIC_TEMPLATE_FOLDER=./alembic_template_sqlite
+ALEMBIC_TEMPLATE_FOLDER_ABSOLUTE_PATH=$(realpath "$ALEMBIC_TEMPLATE_FOLDER")
+
+if [ -e $DB_FILE ]; then
+	uvicorn main:app --host 0.0.0.0 --port 8000
+else
+    alembic init -t "$ALEMBIC_TEMPLATE_FOLDER_ABSOLUTE_PATH" migrations
+    alembic revision --autogenerate
+    alembic upgrade head
+    uvicorn main:app --host 0.0.0.0 --port 8000
+fi
