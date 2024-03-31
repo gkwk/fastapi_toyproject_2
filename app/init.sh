a5214356190d4afd32e4bf342196939930dcd93d
gkwk <124476558+gkwk@users.noreply.github.com>
2024-03-31 17:10:53 +0900

    [Feat] Nginx 추가

--- app/init.sh
+++ app/init.sh
@@ -5,10 +5,10 @@ ALEMBIC_TEMPLATE_FOLDER=./alembic_template_sqlite
 ALEMBIC_TEMPLATE_FOLDER_ABSOLUTE_PATH=$(realpath "$ALEMBIC_TEMPLATE_FOLDER")
 
 if [ -e $DB_FILE ]; then
-	uvicorn main:app --host 0.0.0.0 --port 8000
+	uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers
 else
     alembic init -t "$ALEMBIC_TEMPLATE_FOLDER_ABSOLUTE_PATH" migrations
     alembic revision --autogenerate
     alembic upgrade head
-    uvicorn main:app --host 0.0.0.0 --port 8000
+    uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers
 fi
